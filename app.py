dfad8bae433ed8de99bdc704f4d4ad483a4944d5
Bimal Kumar Shrestha <bimal.stha89@gmail.com>
2024-04-18 12:07:32 -0400

    FrontEnd

---
+++ app.py
@@ -0,0 +1,114 @@
+import streamlit as st
+from st_audiorec import st_audiorec
+import pipeline 
+import sounddevice as sd
+import threading
+import time 
+
+st.set_page_config(
+    page_title='Toxicity Detection',
+    page_icon='🔊'
+)
+
+
+
+st.title('Toxic Audio Detection')
+
+audio_uploaded = st.file_uploader('Upload an audio file', type=['wav', 'mp3', 'm4a'])
+predict_button = st.button('Predict')
+if audio_uploaded:
+    st.audio(audio_uploaded)
+    if predict_button:
+        predictions = pipeline.make_prediction(audio_uploaded)
+        st.write(predictions)
+        start = -1
+        cont = False
+        for i in range(len(predictions)):
+            if start== -1 and predictions[i]==1:
+                st.write('!!! TOXICITY DETECTED !!!')
+                if not cont:
+                    start = i*5
+                cont = True
+            elif start!= -1 and predictions[i] == 0:
+                st.audio(audio_uploaded, start_time = start, end_time = (i)*5)
+                start = -1
+                cont = False
+            st.write(start)
+        if start:
+            st.audio(audio_uploaded, start_time = start, end_time = len(predictions)*5)
+
+
+st.write('OR')
+st.write('Live Audio')
+
+
+
+# Function to record audio chunk
+def record_audio_chunk(chunk_duration=5, fs=22050):
+    chunk_samples = int(chunk_duration * fs)
+    recording = sd.rec(chunk_samples, samplerate=fs, channels=1, dtype='int16')
+    sd.wait()
+    return recording
+
+
+
+# # Function to start recording and playback
+# def record_and_play(chunk_duration, fs):
+#     while True:
+#         st.write("Recording...")
+#         audio_chunk = record_audio_chunk(chunk_duration, fs)
+#         st.write("Playing...")
+#         play_audio(audio_chunk)
+#         st.write("Waiting for next recording...")
+#         time.sleep(chunk_duration)
+
+fs = 22050  # Sample rate
+chunk_duration = 5  # Duration of each audio chunk in seconds
+recording = False
+
+st.title("Audio Recorder")
+
+if st.button("Start Recording"):
+    recording = True
+    while recording:
+        rec = record_audio_chunk()
+        st.audio(rec)
+if st.button("Stop Recording"):
+    recording = False
+
+
+# wav_audio_data = st_audiorec()
+
+# if wav_audio_data is not None:
+#     st.audio(wav_audio_data, format='audio/wav')
+#     if st.button('Examine Recording'):
+#         predictions = pipeline.make_prediction(wav_audio_data, bytes = True)
+#         st.write(predictions)
+# from audio_recorder_streamlit import audio_recorder
+
+# audio_bytes = audio_recorder()
+# if audio_bytes:
+#     st.audio(audio_bytes, format="audio/wav")
+#     if st.button('Examine Recording'):
+#         predictions = pipeline.make_prediction(audio_bytes, bytes = True)
+#         st.write(predictions)
+        
+st.markdown("---")
+st.write("Made with ❤️ by GroupB1")
+
+st.markdown(
+    """
+    <style>
+        body {
+            color: #333;
+            background-color: #f8f9fa;
+        }
+        .st-ba {
+            padding: 1rem;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
---
+++ pipeline.py
@@ -0,0 +1,55 @@
+
+import librosa
+from pydub import AudioSegment
+from io import BytesIO
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model('toxic_audio_model.hdf5')
+
+def convert_to_wav(audio):
+    if audio.type == 'audio/wav':
+        return audio
+    else:
+        audio_data = BytesIO(audio.read())
+        audio_segment = AudioSegment.from_file(audio_data, format=audio.type)
+        wav_data = BytesIO()
+        audio_segment.export(wav_data, format="wav")
+        wav_data.seek(0)
+        return wav_data
+
+def make_prediction(audio):
+    audio = convert_to_wav(audio)
+    mfccs = get_mfccs_from_audio(audio)
+    mfccs = np.array(mfccs)
+    mfccs = mfccs.reshape(mfccs.shape[0], mfccs.shape[1], mfccs.shape[2], 1)
+    prediction = (model.predict(mfccs) > 0.5).astype(int)
+    return prediction
+
+def vol_normalization(audio, max_rms):
+    rms = np.sqrt(np.mean(np.square(audio)))
+    scaling_factor = max_rms / rms
+    normalized_audio = audio * scaling_factor
+    return normalized_audio
+
+
+def get_mfccs_from_audio(audio_path):
+    max_rms = 0.35941255
+    y, sr = librosa.load(audio_path)
+    y = vol_normalization(y, max_rms)
+    mfccs = []
+    start_sample = 0
+    end_sample = len(y)
+    segment_samples = int(5 * sr)
+    
+    while start_sample < end_sample:
+        if start_sample + segment_samples > end_sample:
+            break
+        y_slice = y[start_sample:start_sample + segment_samples]
+        ms = librosa.feature.melspectrogram(y=y_slice, sr=sr)
+        log_ms = librosa.power_to_db(ms, ref=np.max)
+        padded_mfccs = np.pad(log_ms, ((0, 0), (0, max(0, 216 - log_ms.shape[1]))), mode='constant')
+        mfccs.append(padded_mfccs)
+        start_sample += segment_samples
+    
+    return mfccs
